ce8c2674f3e59959afcbb03b910826aba0b64976
laaroubichaymae <laaroubi.chaymae@gmail.com>
2023-07-15 08:59:54 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- *  puzzle followed by newline.
- * Return Always 0. 
+ * puzzle, followed by newline.
+ * Return: Always 0. 
  */
+
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
