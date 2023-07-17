09c597674529b407df67fa4908da8367a57f1557
laaroubichaymae <laaroubi.chaymae@gmail.com>
2023-07-17 07:25:19 -0700

    hi

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,9 +23,9 @@ int main(void)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar((digit3 % 10) + '0')i;
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;				
+				continue;				
 				putchar(',');
 				putchar(' ');
 			}
