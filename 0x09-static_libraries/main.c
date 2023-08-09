49c20920f96647182ca79c0209b4c0d13bb6b372
laaroubichaymae <laaroubi.chaymae@gmail.com>
2023-08-09 09:55:13 -0700

    hi

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a quote by Kevin Mitnick.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-   return (0);
+_puts("\"Attheendoftheday,mygoalwastobethebesthacker\"\n\t- KevinMitnick");
+return (0);
 }
